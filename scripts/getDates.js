7191bd357b843c7165349ca0f61c6dd46402cee3
Devun Durst <devunfoxdurst@yahoo.com>
2024-01-18 17:15:31 -0700

    Webfront-end-site-complete

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,14 @@
 const date = new Date();
 const year = date.getFullYear();
+const author = `Devun Fox Durst`
+const country = `USA`
 
 document.getElementById('dateTime').innerHTML
 =`&copy${year}`;
 document.getElementById('author').innerHTML=`Devun Fox Durst`;
 document.getElementById('country').innerHTML=`USA`;
 
+// javaScript for last Modified
+const LastModif = document.lastModified;
+document.getElementById('modifiedTime').innerHTML
+=`Last modified: ${LastModif}`;
--- scripts/lastMotified.js
+++
@@ -1,3 +0,0 @@
-const LastModif = document.lastModified;
-document.getElementById('modifiedTime').innerHTML
-=`${LastModif}`;
